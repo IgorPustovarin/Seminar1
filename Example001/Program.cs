8c80ea45935fab06b43bbcdb3ebb635e4f7d5906
Igor <adel-time@mail.ru>
2022-10-15 19:55:40 +0800

    Семинар по С#

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+//Число умноженное на само себя.
+Console.WriteLine("Вееди число");
+
+int a = int.Parse(Console.ReadLine()!);
+int b = a * a;
+
+Console.WriteLine(b);
+
---
+++ Seminar001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первого.
+Console.WriteLine("Вееди число1");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Вееди число2");
+
+int b = int.Parse(Console.ReadLine()!);
+
+int c = b / a;
+
+if (c == a)
+{
+    Console.WriteLine("Это квадрат числа");
+}
+else 
+{
+    Console.WriteLine("Не является квадратом");
+}
