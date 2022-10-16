85cd5ce1331a68dd94b3e39b4f5f68e090886fd2
Igor <adel-time@mail.ru>
2022-10-16 16:55:34 +0800

    Семинар2

---
+++ Seminar002/Program.cs
@@ -0,0 +1,15 @@
+﻿int max = new Random().Next(10, 99);
+Console.WriteLine("Получаем случайное число: ");
+Console.WriteLine(max);
+int GetFirstDigit(int max)
+{
+    int number1 = max / 10;
+    return number1;
+}
+int Get2Digit(int max)
+{
+    int number2 = max % 10;
+    return number2;
+}
+if (GetFirstDigit(max)>Get2Digit(max)) Console.WriteLine(GetFirstDigit(max));
+if (GetFirstDigit(max)<=Get2Digit(max)) Console.WriteLine(Get2Digit(max));
