be2dd423213e7341719fcc5c472d812aa0bfb8ec
Igor <adel-time@mail.ru>
2022-10-20 17:32:41 +0800

    Сделал вторую задачу Первого семинара С#

---
+++ HomeWork1C#Seminar1/TaskTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число a");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите_число_b");
+
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число с");
+
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (a < b) max = b;
+if (a < c) max = c;
+//if (b < c) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
