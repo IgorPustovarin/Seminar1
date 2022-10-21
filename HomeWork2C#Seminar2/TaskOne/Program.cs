9449a3192f25c8d7984037fb67c120dd73ee5ebf
Igor <adel-time@mail.ru>
2022-10-22 07:45:52 +0800

    Сделал первую задачу Второго семинара С#

---
+++ HomeWork2C#Seminar2/TaskOne/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число ");
+
+int number = int.Parse(Console.ReadLine()!);
+
+number = number / 10;
+
+    Console.Write("Вторая цифра трехзначного числа ");
+
+Console.Write(number = number % 10);
