7eb6b6febc957435d941ca95d4f6cbda6d14e514
Igor <adel-time@mail.ru>
2022-10-22 08:17:58 +0800

    Сделал вторую задачу Второго семинара С#

---
+++ HomeWork2C#Seminar2/TaskTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+
+while(number > 1000)
+{
+    number = number / 10;
+}
+
+if (99 < number)
+{
+    Console.Write("Третья цифра числа ");
+    Console.WriteLine(number = number % 10);
+}
+else
+{
+    Console.Write("Третьеей цифры в числе нет ");
+}
+
