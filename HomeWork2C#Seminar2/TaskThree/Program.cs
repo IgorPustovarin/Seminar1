807a5563e39063302930ffcd01ca0407f729a49c
Igor <adel-time@mail.ru>
2022-10-22 14:48:05 +0800

    Сделал Третью задачу Второго семинара С#

---
+++ HomeWork2C#Seminar2/TaskThree/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите номер дня недели ");
+int numberday = int.Parse(Console.ReadLine()!);
+
+if (5 < numberday && numberday < 8)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (0 < numberday && numberday < 6)
+{
+    Console.WriteLine("Рабочий день");
+}
