2a7d171311edf26398182625a4e253fa6ad88d27
Igor <adel-time@mail.ru>
2022-10-25 21:27:17 +0800

    Исправил Вторую задачу Первого семинараС#

---
+++ Seminar004/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+int result = 1;
+
+while(count <= number)
+{
+    result = result * count;
+    count ++;
+}
+Console.WriteLine(result);
