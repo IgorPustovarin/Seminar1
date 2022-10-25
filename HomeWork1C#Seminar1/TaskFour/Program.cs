9093d38a0b12851e92d3796f9a51321b08943ac7
Igor <adel-time@mail.ru>
2022-10-25 21:50:07 +0800

    Исправил Четвертую задачу Первого семинараС#

--- HomeWork1C#Seminar1/TaskFour/Program.cs
+++ HomeWork1C#Seminar1/TaskFour/Program.cs
@@ -1,20 +1,12 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 Console.WriteLine("Введите число, увидим все четные числа от 1 до введенного числа ");
 
-int a = int.Parse(Console.ReadLine()!);
+int number = int.Parse(Console.ReadLine()!);
 int count = 2;
-int b = 0;
-b = a % 2;
-
-if (b == 0)
+Console.Write($"Все четные числа от 1 до {number} следующие: ");
+while(count <= number)
 {
-    a = a + 1;
-}
-
-while(count < a)
-{
-    Console.Write("Число ");
-    Console.WriteLine(count);
+    Console.Write($"{count}, ");
     count = count +2;
 }
-Console.WriteLine("Вывели все четные числа в нашем диапазоне");
+Console.WriteLine("Вывели все четные числа в заданном диапазоне");
