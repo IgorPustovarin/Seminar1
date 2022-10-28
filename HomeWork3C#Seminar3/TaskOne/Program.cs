8cf293c6f2f7903a02c0b0a2858af4e6eac42d5a
Igor <adel-time@mail.ru>
2022-10-28 17:00:24 +0800

    Исправил первую задачу Третьего семинараС#

---
+++ HomeWork3C#Seminar3/TaskOne/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int[] inputArray(int n)
+    {
+        int[] array = new int[5];
+        for (int count = 0; count < 5; count++)
+        {
+            array[count] = n % 10;
+            //Console.WriteLine(array[count]);
+            n = n /10;
+        }
+        
+        if (array[0] == array[4])
+        {
+            if(array[1] == array[3])
+                {
+                    Console.WriteLine("Число палиндром");
+                }
+            else 
+            {
+                Console.WriteLine("Число не палиндром");
+            }
+        }
+        else
+        {
+        Console.WriteLine("Число не палиндром");
+        }
+        return array;
+    }
+
+Console.WriteLine("Введите пятизначное число, проверим является оно палиндромом?");
+int number = int.Parse(Console.ReadLine()!);
+inputArray(number);
---
+++ HomeWork3C#Seminar3/TaskTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = 5;
+int b = 2;
+int c = 2;
+int d = 5;
+
+if (n == d)
+    {
+        if(b == c)
+            {
+                Console.WriteLine("Число палиндром");
+            }
+        else 
+        {
+            Console.WriteLine("Число не палиндром");
+        }
+    }
+else
+    {
+        Console.WriteLine("Число не палиндром");
+    }
+
