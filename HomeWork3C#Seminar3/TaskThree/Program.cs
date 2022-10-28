9b5c36399925fe115b026550b8a7dffd6bc9c1a6
Igor <adel-time@mail.ru>
2022-10-28 22:40:37 +0800

    Сделал Третью задачу Третьего семинараС#

---
+++ HomeWork3C#Seminar3/TaskThree/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+int[] mathArray(int n)
+    {
+        int[] array = new int[n];
+        for (int count = 0; count < n; count++)
+        {
+            array[count] = (count + 1) * (count + 1) * (count + 1);
+            Console.Write($"{array[count]}, ");
+        }
+        return array;
+    }
+Console.WriteLine("Введите число, выведу числа от 1 до введенного числа в кубе в кубе");
+int number = int.Parse(Console.ReadLine()!);
+mathArray(number);
