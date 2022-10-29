b4b47929f8fa607b1d57c494de6887c36daf39e8
Igor <adel-time@mail.ru>
2022-10-29 22:31:26 +0800

    Сделал третью задачу Четвертого семинараС#

---
+++ HomeWork4C#Seminar4/TaskThree/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+    for (int count = 0; count < 8; count++)
+    {
+        Console.WriteLine("Введите число, нужно 8 чисел через Enter");
+        int number = int.Parse(Console.ReadLine()!);
+        array[count] = number;
+    }
+    Console.WriteLine("Поздравляю, сформировали массив из 8 элементов");
+    for (int count = 0; count < 8; count++)
+    {
+        Console.Write($"{array[count]}, ");
+    }
