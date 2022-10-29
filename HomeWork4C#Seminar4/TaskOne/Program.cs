72b408565a502baa1dd19b4afb940eeebddfdd61
Igor <adel-time@mail.ru>
2022-10-29 21:53:42 +0800

    Сделал первую задачу Четвертого семинараС#

---
+++ HomeWork4C#Seminar4/TaskOne/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int RaseDegree(int a, int b)
+    {
+        int result = a;
+        for (int count = 1; count < b; count++)
+        {
+            result = result * a;
+        }
+        Console.WriteLine($"Возвели число А в степень В получили {result}");
+        return result;
+        
+    }
+
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число В");
+int B = int.Parse(Console.ReadLine()!);
+RaseDegree(A, B);
