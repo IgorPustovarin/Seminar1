8062ffd8dd5da1440c62c4dceab0c88ff457a0b0
Igor <adel-time@mail.ru>
2022-10-29 22:07:27 +0800

    Сделал вторую задачу Четвертого семинараС#

---
+++ HomeWork4C#Seminar4/TaskTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int allSumNumber(int n)
+    {
+        int result = 0;
+        for (int count = 0; count < 10; count++)
+        {
+            result = result + n % 10;
+            //Console.WriteLine(array[count]);
+            n = n /10;
+        }
+        Console.WriteLine($"Сумма всех цифр {result}");
+        return result;
+    }
+Console.WriteLine("Введите число и посчитаем сумму цифр в нем");
+int number = int.Parse(Console.ReadLine()!);
+allSumNumber(number);
