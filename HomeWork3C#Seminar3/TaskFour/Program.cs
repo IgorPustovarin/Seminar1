2a01003bb5c440f807662ddb6fdfe376ef1876cd
Igor <adel-time@mail.ru>
2022-10-29 11:04:12 +0800

    Почти сделал четвертую задачу Третьего семинараС#

---
+++ HomeWork3C#Seminar3/TaskFour/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
+
+
+int Proverka(int n)
+    {
+        int count = n;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (i%j ==0) count++;
+            if (count == 2) Console.WriteLine($"{i}, ");
+                
+            }
+        }
+        return count;
+    } 
+Console.WriteLine("Введите число и найдем все простые числа от 1 до этого числа");
+int number = int.Parse(Console.ReadLine()!);
+Proverka(number);
