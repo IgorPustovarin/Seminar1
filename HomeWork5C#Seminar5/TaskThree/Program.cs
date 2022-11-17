76215deb90dffe2be3ed977d18b7cc12ff35ac08
Igor <adel-time@mail.ru>
2022-11-17 21:56:42 +0800

    Сделал третью задачу пятого семинараС#

---
+++ HomeWork5C#Seminar5/TaskThree/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] array = new int[5];
+        //array[0] = 1;
+    for (int count = 0; count < 5; count++)
+    {
+        array[count] = new Random().Next(1, 20);
+        Console.Write($"{array[count]}, ");
+    }
+          int minNumber = array[0];
+          int maxNumber = array[0];
+          for (int count = 0; count < 5; count++)
+    {
+        if (array[count] > maxNumber)
+        {
+            maxNumber = array[count];
+        }
+        if (array[count] < minNumber)
+        {
+            minNumber = array[count];
+        }
+    }
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным {maxNumber} и минимальным {minNumber} элементом массива равна {maxNumber - minNumber}");
