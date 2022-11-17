439abb0e2cd632316a591412db35e48e3e0f85cd
Igor <adel-time@mail.ru>
2022-11-17 18:43:53 +0800

    Сделал первую задачу пятого семинараС#

---
+++ HomeWork5C#Seminar5/TaskOne/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+int NumberPositive = 0;
+int[] Array = new int [10];
+    for (int count = 0; count < 10; count++)
+    {
+        Array[count] = new Random().Next(100, 999);
+        if (Array[count] % 2 == 0)
+        {
+            NumberPositive = NumberPositive + 1;
+        }
+        Console.Write($"{Array[count]}, ");
+    }
+Console.WriteLine($"Количество четных чисел {NumberPositive}");
+
