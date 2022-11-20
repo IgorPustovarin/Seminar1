96e0ad2677ba094b388f4f63948e44c4973e4388
Igor <adel-time@mail.ru>
2022-11-20 09:13:45 +0800

    Сделал первую задачу шестого семинараС#

---
+++ HomeWork6C#Seminar6/TaskOne/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Сколько чисел планируте ввести?");
+int quantity = int.Parse(Console.ReadLine()!);
+int countPositiveNumbers = 0;
+for (int i = 0; i < quantity; i++)
+    {
+        Console.WriteLine("Вводите числа чечез Enter");
+        int number = int.Parse(Console.ReadLine()!);
+        if (number > 0)
+        {
+            countPositiveNumbers++;
+        }
+    }
+Console.WriteLine($"Количество положительных чисел {countPositiveNumbers}");
