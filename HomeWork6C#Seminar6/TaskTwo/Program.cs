6f6d691f4b019d62f02a8f06196361bf8e3495fd
Igor <adel-time@mail.ru>
2022-11-20 09:52:03 +0800

    Сделал вторую задачу шестого семинараС#

--- HomeWork6C#Seminar6/TaskTwo/Program.cs
+++ HomeWork6C#Seminar6/TaskTwo/Program.cs
@@ -2,10 +2,15 @@
 значения b1, k1, b2 и k2 задаются пользователем.
 Например при значениях b1 = 2, k1 = 5, b2 = 4, k2 = 9 Ответ: (-0,5; -0,5)
 */
-float b1 = 2;
-float k1 = 5;
-float b2 = 4;
-float k2 = 9;
+Console.WriteLine("Введите b1");
+float b1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1");
+float k1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2");
+float b2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2");
+float k2 = int.Parse(Console.ReadLine()!);
+
 float x = 0;
 float y = 0;
 if (k1 == k2)
@@ -17,5 +22,4 @@ else
         x = (b2 - b1)/(k1 - k2);
         y = k1*x +b1;
     }
- 
-Console.WriteLine($"Координаты пересечения прямых X = {x}, Y = {y}");
+ Console.WriteLine($"Координаты пересечения прямых X = {x}, Y = {y}");
