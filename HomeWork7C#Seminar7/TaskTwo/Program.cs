9f1fa1fbdc7c70989424dd0d6dba76f68a71a25c
Igor <adel-time@mail.ru>
2022-11-26 10:17:54 +0800

    Сделал вторую задачу седьмого семинараС#

---
+++ HomeWork7C#Seminar7/TaskTwo/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void PrintArray(int[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i,j] = new Random().Next(1,10);
+        
+        }
+    }
+}
+
+void SearchIndex(int[,] mas)
+{
+    Console.WriteLine("Нужно ввести через Enter первый индекс массива");
+    int indexOne = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Нужно ввести второй индекс массива");
+    int indexTwo = int.Parse(Console.ReadLine()!);
+
+    if (indexOne < mas.GetLength(0) && indexTwo < mas.GetLength(1))
+    {
+        Console.Write($"Под данным индексом массива содержится {mas[indexOne, indexTwo]}");
+    }
+    else
+    {
+        Console.Write("Данного индекса не существует");
+    }
+}
+int[,] matrix = new int [4, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+SearchIndex(matrix);
+
+
+
+
