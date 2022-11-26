63638da491a52e1d774c3f726ffcaeb36034c5df
Igor <adel-time@mail.ru>
2022-11-26 09:51:29 +0800

    Сделал первую задачу седьмого семинараС#

---
+++ HomeWork7C#Seminar7/TaskOne/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void PrintArray(double[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[i, j]:F2} ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(double[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i,j] = new Random().NextDouble() * 100;
+        
+        }
+    }
+}
+double[,] matrix = new double [4, 4];
+FillArray(matrix);
+PrintArray(matrix);
