8fc723d5132e511a3941c1693b89f1066dfcfc7e
Igor <adel-time@mail.ru>
2022-11-27 09:58:34 +0800

    Сделал третью задачу восьмого семинараС#

---
+++ HomeWork8C#Seminar8/TaskThree/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+int [,] matrix1 = new int[,]
+{
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4}
+};
+
+void CompositionMatrix(int[,] mas)
+{
+    int [,] matrix2 = new int[,]
+    {
+        {2,2,2,2},
+        {2,2,2,2},
+        {2,2,2,2}
+    };
+    for (int i=0; i < mas.GetLength(0); i++)
+    {
+        for (int j=0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = mas[i,j] * matrix2[i,j];
+        }
+    }
+}
+
+void PrintArray(int[,] mas)
+{
+    for (int i =0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+PrintArray(matrix1);
+CompositionMatrix(matrix1);
+Console.WriteLine();
+PrintArray(matrix1);
